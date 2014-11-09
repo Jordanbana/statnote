6d5be87e7051f270fdac394f23d6414a29f3ed90
Jordanbana <jordanbana@gmail.com>
2014-11-08 18:21:03 -0800

    web loading on server

--- hello.py
+++ hello.py
@@ -1,33 +1,10 @@
-# from flask import Flask
-# app = Flask(__name__)
-#
-# @app.route("/")
-# def hello():
-#     return websites.html
-#
-# if __name__ == "__main__":
-#     app.run()
-
-
-# from flask import Flask, request
-# # set the project root directory as the static folder, you can set others.
-# app = Flask(__name__, static_url_path='')
-#
-# @app.route('/')
-# def root():
-#     return app.send_static_file('website.html')
-#
-#
-# if __name__ == "__main__":
-#     app.run()
-
-
 from flask import Flask, request
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__, static_url_path='/static')
 
-@app.route('/index/')
+@app.route('/')
 def root():
-    return app.send_static_file('websites.html')
+    return app.send_static_file('website.html')
+    #return """<html><body><p>hello world</p></body></html>"""
 
 if __name__ == '__main__':
   app.run()
